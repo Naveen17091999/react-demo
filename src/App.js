0544f79494fcb1504508a914ccb1db793976d4d7
naveen <welbe1709@gmail.com>
2024-05-28 11:44:42 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,73 @@
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import Banner from './components/banner/Banner';
 import { Footer } from './components/footer/Footer';
 import Courses from './components/market/market';
 import Navbar from './components/navbar/navbar';
+import About from './components/about/About';
+import Cart from './components/cart/Cart';
+import Notification from './components/notification/Notification';
+import Profile from './components/profile/Profile';
+import Login from './components/login/Login';
 
-function App() {
-  
+
+function Layout() {
   return (
-    <div className="App">
+    <div className="layout">
       <Navbar />
-      <Banner />
-      <Courses />
+      <Outlet />
       <Footer />
     </div>
+  )
+}
+
+function Home() {
+  return (
+    <div className="home">
+      <Banner />
+      <Courses />
+    </div>
+  )
+}
+
+
+function App() {
+
+  const routingPath = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element:<Home/>
+        },
+        {
+          path:'about',
+          element:<About/>
+        },
+        {
+          path:'cart',
+          element:<Cart/>
+        },
+        {
+          path:'notification',
+          element:<Notification/>
+        },
+        {
+          path:'profile',
+          element:<Profile/>
+        },
+        {
+          path:'login',
+          element:<Login/>
+        },
+      ]
+    }
+  ])
+
+  return (
+    <RouterProvider router={routingPath}/>
   );
 }
 
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,23 +1,25 @@
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
-function Navbar(){
 
-    function btn (){
+function Navbar() {
+
+    function btn() {
         alert("clicked")
     }
-    return(
+    return (
         <nav id="Navbar">
             <div className="logo">Logo</div>
 
             <ul>
-                <li onClick={btn}>Home</li>
-                <li>about</li>
-                <li>cart</li>
-                <li>Notification</li>
-                <li>Profile</li>
-                <li>login</li>
+                <Link to="/"><li >Home</li></Link>
+                <Link to="about"><li>about</li></Link>
+                <Link to="cart"><li>cart</li></Link>
+                <Link to="Notification"><li>Notification</li></Link>
+                <Link to="Profile"><li>Profile</li></Link>
+                <Link to="login"><li>login</li></Link>
             </ul>
-            
+
         </nav>
     )
 }
